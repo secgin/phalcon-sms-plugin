1dcbf40227e75416cf5d4d71bec8534d605fdd3f
Seçgin ŞANLI <secgin@Secgin-MacBook-Pro.local>
2023-08-21 15:41:15 +0300

    ClientProvider içerisinde sms default ayarları düzenlemesi

--- src/ClientProvider.php
+++ src/ClientProvider.php
@@ -9,10 +9,20 @@ class ClientProvider implements ServiceProviderInterface
 {
     public function register(DiInterface $di): void
     {
-        $config = $di->getShared('config')->get('sms')->toArray();
+        $smsConfig = $di->getShared('config')->get('sms')->toArray();
 
-        $di->setShared('sms', function() use ($config) {
-            return new Client($config);
+        $di->setShared('sms', function () use ($smsConfig) {
+            $options = [
+                'username' => $smsConfig['username'],
+                'password' => $smsConfig['password'],
+                'defaultMessageHeader' => $smsConfig['defaultMessageHeader'],
+                'provider' => $smsConfig['provider']
+            ];
+
+            unset($smsConfig['username'], $smsConfig['password'], $smsConfig['defaultMessageHeader'],
+                $smsConfig['provider']);
+
+            return new Client($options, $smsConfig);
         });
     }
 }
